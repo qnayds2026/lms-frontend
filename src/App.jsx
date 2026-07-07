6f42a3637a7420598241dc34c4ce29e5e65eec86
gopikac2312-collab <gopikac2312@gmail.com>
2026-07-07 14:18:43 +0530

    Merge remote changes into App.jsx, add loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,28 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layoutes/Layout";
 import Home from "./pages/Home";
-<<<<<<< HEAD
-import Login from "./pages/Login";
 import About from "./pages/About";
-import Courses from "./pages/Courses"
- 
-=======
+import Courses from "./pages/Courses";
+
+import Login from "./pages/Login";
 import Register from "./pages/Register";
->>>>>>> aed10666d892e201c416a8257473c52dca562b57
+
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-           <Route path="about" element={<About />} />
-           <Route path="courses"element={<Courses/>}/>
-          </Route>
-        </Routes>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
-    </>
+          <Route path="about" element={<About />} />
+          <Route path="courses" element={<Courses />} />
+        </Route>
+
+        {/* Auth pages rendered standalone, without the public Navbar/Footer */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 };
 
